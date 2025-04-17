0fab32200b2e339079ea4ccc8fb9cb8155d5c588
sangdo14 <sangdo14@naver.com>
2025-04-17 16:41:38 +0900

    feat: 원안으로 텍스트 이동

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const txt = document.querySelector('.txt');
+const innerText = document.querySelector('.innerText');
+const section = document.querySelector("section");
 
 window.addEventListener('scroll', ()=>{
   const scroll = window.scrollY;
   console.log(scroll);
   txt.style.left = 100 - scroll/5 + '%';
+  innerText.style.left = 100 - scroll/5 + '%';
+  section.style.clipPath = `circle(${scroll/5}px at center)`;
 });
+
